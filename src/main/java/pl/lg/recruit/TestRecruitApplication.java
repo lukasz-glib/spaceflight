a478f25c1aee1ad01c8dbb8f8de7e46f0358fd8e
Lukasz Glib <lukasz.gl28@gmail.com>
2020-02-17 20:50:03 +0100

    add configuration

---
+++ src/main/java/pl/lg/recruit/TestRecruitApplication.java
@@ -0,0 +1,12 @@
+package pl.lg.recruit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestRecruitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestRecruitApplication.class, args);
+    }
+}
